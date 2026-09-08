466eb88a77a641270b0966a5b089e17616d5a7b7
tiwarishubham9721-gif <tiwarishubham9721@gmail.com>
2026-09-08 10:26:00 +0530

    Delete android/public-app/src/main/java/com/khabardhun/publicapp/MainActivity.java

--- android/public-app/src/main/java/com/khabardhun/publicapp/MainActivity.java
+++
@@ -1,3 +0,0 @@
-package com.khabardhun.publicapp;
-import android.app.Activity; import android.os.Bundle; import android.webkit.WebView; import android.webkit.WebViewClient;
-public class MainActivity extends Activity { public void onCreate(Bundle b){super.onCreate(b); WebView w=new WebView(this); w.setWebViewClient(new WebViewClient()); w.getSettings().setJavaScriptEnabled(true); w.loadUrl(getString(com.khabardhun.publicapp.R.string.backend_url)+"/app"); setContentView(w);} }
